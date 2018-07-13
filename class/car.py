bb519a462c9be40e06685a15e7095817b23b0d26
Iftekhar Chowdhury <33630256+TanmoyCtg@users.noreply.github.com>
2018-07-13 19:50:01 +0600

    updated car class

--- class/car.py
+++ class/car.py
@@ -1,20 +1,14 @@
->>> import turtle
->>> tom = turtle.Turtle()
->>> tom.circle(100)
->>> type(tom)
-<class 'turtle.Turtle'>
->>> nonte = turtle.Turtle()
->>> fonte = turtle.Turtle()
->>> nonte.shape("circle")
->>> fonte.shape("square")
->>> nonte.left(40)
->>> nonte.forward(100)
->>> fonte.backward(200)
->>> monte = turtle.Turtle()
->>> monte.setpos(-200,-200)
->>> monte.forward(30)
->>> monte.clear()
->>> nonte.clear ()
->>> fonte.clear()
->>> fonte.shape("triangle")
->>> monte.shape("square")
+class Car:
+  
+  name =  "Premio"
+  color= "white"
+  
+  def start():
+    x= "Starting the engine"
+    print (x)
+  
+  
+print("Name of the car:",Car.name)
+print("Color:",Car.color)
+
+Car.start()
